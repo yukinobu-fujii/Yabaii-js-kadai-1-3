89e2ea54dfe73ab5bb5788a722d54c3ad67b042d
yukinobu fujii <y.f.mynovember0818@gmail.comgit config --global user.email y.f.mynovember0818@gmail.com>
2022-07-27 20:38:33 +0900

    課題更新インデックなど

--- kadai1-3.js
+++ kadai1-3.js
@@ -1,51 +1,27 @@
+const taskMenu = [
+    '==========================',
+    '現在持っているのタスク一覧',
+    '==========================',
+];
+console.log( taskMenu.join('\n') );
 
-const task = function taskMenu() {
-    console.log('==========================')
-    console.log('現在持っているのタスク一覧')
-    console.log('==========================')
-}
-task();
-
-let taskNames = [
+const taskNames = [
     '掃除',
     '買い物',
     '徒歩',
 ];
-const task_names = function taskMenu() {
-    taskNames.forEach(function( value ) {
-        console.log( value );
+const taskShow = () => {
+    taskNames.forEach(( value, index) => {
+        console.log( index, value);
     });
 }
-task_names();
+taskShow();
 while(true){
     if(taskNames.push(prompt('「確認、追加、削除、終了」のいずれかを入力してください')));
     {
-        task();
-        task_names();
+        console.log( taskMenu.join('\n') );
+        taskShow();
         alert(`新しいタスクを追加しました。`);
         break;
     }
 }
-
-/*
-let alltaskNames = taskNames.map(taskName=> {
-  return taskName
-});
-console.log('==========================')
-console.log('現在持っているのタスク一覧')
-console.log('==========================')
-
-console.table(alltaskNames);
-
-let task = { index:'0', value:'掃除'}
-console.log(task);
-
-while(true){
-    if(prompt(quizCapitalJapan)=== '東京'){
-      alert(`${correctAnswer}です！`);
-      break;
-    }else{
-    alert(`${incorrectAnswer}です！`);
-    }
-  }
-*/
